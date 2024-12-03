948e9e081792a5f28651eb13ead945c61ab3ef41
Chris Janowiak <janowiak.cr@gmail.com>
2024-12-03 14:34:46 -0500

    Init day 2 with tests.

---
+++ Day2.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Day2;
+
+namespace Day2.Test;
+
+public class Tests
+{
+
+    [Test]
+    public void Test1()
+    {
+        
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    
+}
+
+static class Utils {
+    static bool ReportSafety(IEnumerable<int> report) {
+        throw new NotImplementedException();
+    }
+}
