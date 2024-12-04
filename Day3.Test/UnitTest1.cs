726b67e3aba18023c1e276980f316aa5a589c5b0
Chris Janowiak <janowiak.cr@gmail.com>
2024-12-04 02:09:09 -0500

    day 3 init, always start with a failing test lol

---
+++ Day3.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Day3.Test;
+using static Day3.Utils;
+public class Tests
+{
+
+    [Test]
+    public void Test1()
+    {
+        Assert.That(F(), Is.True);
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day3;
+using Spectre.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public static class Utils {
+    public static bool F() {
+        throw new NotImplementedException();
+    }
+}
