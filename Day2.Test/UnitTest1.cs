e1b72b9e1a66692f8b7c02c4cdf61433ec90fd5b
Chris Janowiak <janowiak.cr@gmail.com>
2024-12-04 01:59:32 -0500

    kludgy solution to day 2

--- Day2.Test/UnitTest1.cs
+++ Day2.Test/UnitTest1.cs
@@ -1,13 +1,62 @@
-﻿using Day2;
+﻿using static Day2.Utils;
 
 namespace Day2.Test;
 
 public class Tests
 {
+    [Test]
+    [TestCase(new int[] {7,6,4,2,1}, true)]
+    [TestCase(new int[] {1,2,7,8,9}, false)]
+    [TestCase(new int[] {9,7,6,2,1}, false)]
+    [TestCase(new int[] {1,3,2,4,5}, false)]
+    [TestCase(new int[] {8,6,4,4,1}, false)]
+    [TestCase(new int[] {1,3,6,7,9}, true)]
+    public void PartOneSamples(int[] report, bool result) {
+        Assert.That(ReportSafety(report), Is.EqualTo(result), "");
+    }
 
     [Test]
-    public void Test1()
-    {
-        
+    [TestCase(new int[] {7,6,4,2,1}, true)]
+    [TestCase(new int[] {1,2,7,8,9}, false)]
+    [TestCase(new int[] {9,7,6,2,1}, false)]
+    [TestCase(new int[] {1,3,2,4,5}, true)]
+    [TestCase(new int[] {8,6,4,4,1}, true)]
+    [TestCase(new int[] {1,3,6,7,9}, true)]
+    public void PartTwoSamples(int[] report, bool result) {
+        Assert.That(ReportSafetyWithDamper(report), Is.EqualTo(result), "");
+    }
+    [Test]
+    [TestCase(new int[] {6,8,11,12,14,16,18,16}, true)]
+    [TestCase(new int[] {73,76,79,80,81,84,86,86}, true)]
+    [TestCase(new int[] {9,11,13,14,17,24}, true)]
+    [TestCase(new int[] {59,61,64,62,65}, true)]
+    [TestCase(new int[] {34,35,42,43,44,51}, false)]
+    [TestCase(new int[] {80,79,80,81,84,86,88,88}, false)]
+    [TestCase(new int[] {14,12,13,14,17,18,21,28}, false)]
+    [TestCase(new int[] {55,52,49,52,55,58,59,63}, false)]
+    [TestCase(new int[] {39,37,44,46,49,54}, false)]
+    [TestCase(new int[] {31,31,32,33,36,39,40,42}, true)]
+    [TestCase(new int[] {6,6,7,10,11,14,16,14}, false)]
+    [TestCase(new int[] {81,78,76,74,73,71,73}, true)]
+    [TestCase(new int[] {78,75,72,71,70,67,67}, true)]
+    [TestCase(new int[] {27,25,22,24,24}, false)]
+    [TestCase(new int[] {95,93,90,93,87}, true)]
+    [TestCase(new int[] {79,77,75,74,71,71,71}, false)]
+    [TestCase(new int[] {36,34,34,32,27}, false)]
+    [TestCase(new int[] {53,54,56,55,58}, true)]
+    [TestCase(new int[] {81,82,77,75,73,70}, false)]
+    [TestCase(new int[] {67,67,66,64,63,60}, true)]
+    public void PartTwoOthers(int[] report, bool result) {
+        Assert.That(ReportSafetyWithDamper(report), Is.EqualTo(result), "");
+    }
+    [Test]
+    [TestCase(new int[] {12,10,11,14,17,18,20,21}, true)]
+    [TestCase(new int[] {24,25,24,22,21,18,15,13}, true)]
+    [TestCase(new int[] {50,48,49,50,53,56}, true)]
+    [TestCase(new int[] {56,58,57,55,54,52}, true)]
+    [TestCase(new int[] {91,92,88,86,85,82,81}, true)]
+    [TestCase(new int[] {19,22,20,19,18}, true)]
+    public void PartTwoMostTricky(int[] report, bool result) {
+        Assert.That(ReportSafetyWithDamper(report), Is.EqualTo(result), "");
     }
 }
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,17 +1,151 @@
 ﻿namespace Day2;
+using Spectre.Console;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using StreamReader r = new("input.txt");
+        var txt = r.ReadToEnd();
+        r.Close();
+        int safes = 0;
+        int safesdamp = 0;
+        foreach(string reportline in txt.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+        {
+            var report = reportline.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s));
+            bool safe = Utils.ReportSafety(report);
+            bool safedamp = Utils.ReportSafetyWithDamper(report.ToList());
+            AnsiConsole.Write($"  {reportline} : ");
+            if(safe) {
+                AnsiConsole.Markup("[Green]SAFE[/]");
+                safes += 1;
+            }
+            else {
+                AnsiConsole.Markup("[Red]UNSAFE[/]");
+            }
+            if(safedamp) {
+                AnsiConsole.Markup(" | With Damper: [Green4]SAFE[/]");
+                safesdamp += 1;
+            }
+            else {
+                AnsiConsole.Markup(" | With Damper: [Red3]UNSAFE[/]");
+            }
+            AnsiConsole.WriteLine();
+        }
+        AnsiConsole.Write(new Rule());
+        AnsiConsole.MarkupInterpolated($"  COUNT OF SAFE REPORTS: [Lime]{safes}[/]");
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupInterpolated($"  WITH DAMPER: [gold3]{safesdamp}[/]");
+        AnsiConsole.WriteLine();
     }
-
-    
 }
 
-static class Utils {
-    static bool ReportSafety(IEnumerable<int> report) {
-        throw new NotImplementedException();
+public static class Utils {
+    public static bool ReportSafety(IEnumerable<int> report) {
+        int? lag = null;
+        int? sign = null;
+        foreach(int current in report) {
+            if(lag is null) // current is first
+            {
+                lag = current;
+                continue;
+            }
+            if(!Safe(lag.Value, current, ref sign))
+                 return false;
+            lag = current; // prep for next iter
+        }
+        return true;
+    }
+    public static bool ReportSafetyWithDamper(IList<int> report, bool recurse = true) {
+        bool damperUsed = !recurse;
+        bool fail = false;
+        int? sign = null;
+        LinkedList<int> q = new();
+        q.AddFirst(report[0]);
+        int i = 1;
+        var w = q.First;
+        while(i < report.Count) 
+        {
+            if(Safe(w?.Value, report[i], ref sign))
+            {
+                q.AddLast(report[i]);
+                w = w.Next;
+                i += 1;
+                continue;
+            }
+            else if(damperUsed)
+            {
+                fail = true;
+                break;
+            }
+            else if(Safe(w?.Previous?.Value, report[i], ref sign)) // try skip last addition
+            {
+                w = w?.Previous;
+                q.RemoveLast();
+                q.AddLast(report[i]);
+                w = w?.Next ?? q.Last;
+                i += 1;
+                damperUsed = true;
+                continue;
+            }
+            else 
+            {
+                damperUsed = true;
+                i += 1;
+                continue;
+            }
+        }
+        if(fail && recurse)
+        {
+            // kludgy. but goddammit it's 2 am and i need to go to sleep
+            var r = report.Skip(1).ToList();
+            var r2 = report.Skip(2).Prepend(report.First()).ToList();
+            bool dumbcheck = ReportSafetyWithDamper(r, false);
+            bool dumbcheck2 = ReportSafetyWithDamper(r2, false);
+            if(dumbcheck)
+            {
+                fail = false;
+                q = new LinkedList<int>(r);
+            }
+            else if(dumbcheck2)
+            {
+                fail = false;
+                q = new LinkedList<int>(r2);
+            }
+        }
+        var t = q.First;
+        AnsiConsole.Write(":");
+        foreach(int n in report)
+        {
+            AnsiConsole.Write(" ");
+            if(n == t?.Value)
+            {
+                AnsiConsole.Write(
+                    new Text($"{n}", new Style(Color.Aqua, Color.Black))
+                );
+                t = t.Next;
+            }
+            else
+            {
+                AnsiConsole.Write(
+                    new Text($"{n}", new Style(Color.DarkCyan, Color.Black, Decoration.Strikethrough))
+                );
+            }
+        }
+        AnsiConsole.WriteLine();
+        return !fail;
+    }
+    public static bool Safe(int? x1, int? x2, ref int? sign) {
+        if(x1 is null || x2 is null) return true;
+        if(x1 == x2) return false;
+        if(sign is null) {
+            sign = (x1 < x2) ? 1 : -1;
+            return Math.Abs(x1.Value - x2.Value) < 4;
+        }
+        if(sign == 1) return x2 > x1 && Math.Abs(x1.Value - x2.Value) < 4;
+        else return x2 < x1 && Math.Abs(x1.Value - x2.Value) < 4;
     }
 }
